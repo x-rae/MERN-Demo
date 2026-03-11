eeff46c0fe0392831acf64e4f2e88764761270b2
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-10 21:03:47 -0400

    create LoggedInName.tsx

---
+++ frontend/src/components/LoggedInName.tsx
@@ -0,0 +1,17 @@
+function LoggedInName()
+{
+    function doLogout(event:any) : void
+    {
+        event.preventDefault();
+        alert('doLogout');
+    };
+
+    return(
+        <div id="loggedInDiv">
+            <span id="userName">Logged In As John Doe</span><br />
+            <button type="button" id="logoutButton" className="buttons" onClick={doLogout}>Log Out</button>
+        </div>
+    );
+};
+
+export default LoggedInName;
