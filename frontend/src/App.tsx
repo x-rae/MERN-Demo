a806a09c575572f5482a58ad622d1194e872055e
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-12 17:33:56 -0400

    add basic routing support to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
 import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
 
 function App()
 {
   return(
-    <LoginPage />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/cards" element={<CardPage />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
