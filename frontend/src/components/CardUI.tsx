ae17af615d59ce32fef657319530972d4c1b2777
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-22 16:19:29 -0400

    add input field interactivity
    for now just brings up alert boxes

--- frontend/src/components/CardUI.tsx
+++ frontend/src/components/CardUI.tsx
@@ -1,27 +1,46 @@
+import React, {useState} from 'react';
+
 function CardUI()
 {
+    //idk why some of these are just useState instead of React.useState
+    const [message, setMessage] = useState('');
+    const [searchResults, setResults] = useState('');
+    const [cardList, setCardList] = useState('');
+    const [search, setSearchValue] = React.useState('');
+    const [card, setCardNameValue] = React.useState('');
+
     function addCard(event:any) : void
     {
         event.preventDefault();
-        alert('addCard()');
+        alert('addCard() ' + card);
     };
 
     function searchCard(event:any) : void
     {
         event.preventDefault();
-        alert('searchCard()');
+        alert('searchCard() ' + search);
     };
 
+    function handleSearchTextChange(e:any) : void
+    {
+        setSearchValue(e.target.value);
+    }
+
+    function handleCardTextChange(e:any) : void
+    {
+        setCardNameValue(e.target.value);
+    }
+
     return(
-        <div id="accessUIDiv">
+        <div id="CardUIDiv">
             <br />
-            <input type="text" id="searchText" placeholder="Card To Search For" />
+            Search: <input type="text" id="searchText" placeholder="Card To Search For" onChange={handleSearchTextChange}/>
             <button type="button" id="searchCardButton" className="buttons" onClick={searchCard}>Search Card</button><br />
-            <span id="cardSearchResult"></span>
-            <p id="cardList"></p><br /><br />
-            <input type="text" id="cardText" placeholder="Card To Add" />
+            <span id="cardSearchResult">{searchResults}</span>
+            <p id="cardList">{cardList}</p><br /><br />
+            Add: <input type="text" id="cardText" placeholder="Card To Add" onChange={handleCardTextChange}/>
             <button type="button" id="addCardButton" className="buttons" onClick={addCard}>Add Card</button><br />
-            <span id="cardAddResult"></span>
+            <span id="cardAddResult">{message}</span>
         </div>
     );
 }
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,35 @@
+import React, {useState} from 'react';
+
 function Login()
 {
+    //idk why one of these is just useState instead of React.useState
+    const [message, setMessage] = useState('');
+    const [loginName, setLoginName] = React.useState('');
+    const [loginPassword, setPassword] = React.useState('');
+
     function doLogin(event:any) : void
     {
         event.preventDefault();
-        alert('doIt()');
+        alert('doIt() ' + loginName + ' ' + loginPassword);
+    }
+
+    function handleSetLoginName(e: any) : void
+    {
+        setLoginName(e.target.value);
+    }
+
+    function handleSetPassword(e: any) : void
+    {
+        setPassword(e.target.value);
     }
 
     return(
         <div id="loginDiv">
             <span id="inner-title">PLEASE LOG IN</span><br />
-            <input type="text" id="loginName" placeholder="Username" /><br />
-            <input type="password" id="loginPassword" placeholder="Password" /><br />
+            Login: <input type="text" id="loginName" placeholder="Username" onChange={handleSetLoginName}/><br />
+            Password: <input type="password" id="loginPassword" placeholder="Password" onChange={handleSetPassword}/><br />
             <input type="submit" id="loginButton" className="buttons" value="Do It" onClick={doLogin} />
-            <span id="loginResult"></span>
+            <span id="loginResult">{message}</span>
         </div>
     );
 };
