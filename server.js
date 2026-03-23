04fd3696518616159492924577330f09ca8fe954
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-22 23:06:47 -0400

    create package.json and server.js in project root
    for backend, I think? package-lock auto created.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.listen(5000); //start Node + Express server on port 5000
