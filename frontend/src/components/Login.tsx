c57a73f8a2b36236b54ace2b58860f93755aebc7
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-23 20:10:05 -0400

    add functionality to doLogin
    also make it async

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -7,11 +7,38 @@ function Login()
     const [loginName, setLoginName] = React.useState('');
     const [loginPassword, setPassword] = React.useState('');
 
-    function doLogin(event:any) : void
+    async function doLogin(event:any) : Promise<void>
     {
         event.preventDefault();
-        alert('doIt() ' + loginName + ' ' + loginPassword);
-    }
+
+        var obj = {login:loginName,password:loginPassword};
+        var js = JSON.stringify(obj);
+
+        try
+        {
+            const response = await fetch('http://localhost:5000/api/login', {method:'POST', body:js, headers:{'Content-Type':'application/json'}});
+
+            var res = JSON.parse(await response.text());
+
+            if(res.id <= 0)
+            {
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                var user = {firstName:res.firstName, lastName:res.lastName, id:res.id};
+                localStorage.setItem('user_data', JSON.stringify(user));
+
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch(error:any)
+        {
+            alert(error.toString());
+            return;
+        }
+    };
 
     function handleSetLoginName(e: any) : void
     {
