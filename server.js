2f11f9668f46165ef976ad34796b0f60736396ae
Rae <60368869+x-rae@users.noreply.github.com>
2026-03-24 19:07:01 -0400

    add mongoDB access

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const client = new MongoClient(url);
+client.connect();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
